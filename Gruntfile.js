5160e4eab39a7a04e67c75c5c20fa520fb89fdd2
Ajmal Afif <ajmalafif@gmail.com>
2013-11-02 00:43:58 +0800

    testing out minify files for shopify

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,37 +6,44 @@ module.exports = function(grunt) {
         files: [
           {
             expand: true, 
-            cwd: 'source/bootstrap/js/', 
-            src: ['*.js'], 
+            flatten: true,
+            src: ['source/bootstrap/dist/**/*.min.*', 'source/bootstrap/fonts/*'], 
             dest: 'theme/assets/'
+          },
+          {
+            expand: true, 
+            flatten: true,
+            src: 'source/shopify-skeleton-theme/templates/*.liquid', 
+            dest: 'theme/templates/'
           }
+
         ]
       }
-    },
-    concat: {
-        bootstrap: {
-          src: [
-            'source/bootstrap/js/transition.js',
-            'source/bootstrap/js/alert.js',
-            'source/bootstrap/js/button.js',
-            'source/bootstrap/js/carousel.js',
-            'source/bootstrap/js/collapse.js',
-            'source/bootstrap/js/dropdown.js',
-            'source/bootstrap/js/modal.js',
-            'source/bootstrap/js/tooltip.js',
-            'source/bootstrap/js/popover.js',
-            'source/bootstrap/js/scrollspy.js',
-            'source/bootstrap/js/tab.js',
-            'source/bootstrap/js/affix.js'
-          ],
-          dest: 'theme/assets/bootstrap.js'
-        }
-    },
+    }
+    //, concat: {
+    //     bootstrap: {
+    //       src: [
+    //         'source/bootstrap/js/transition.js',
+    //         'source/bootstrap/js/alert.js',
+    //         'source/bootstrap/js/button.js',
+    //         'source/bootstrap/js/carousel.js',
+    //         'source/bootstrap/js/collapse.js',
+    //         'source/bootstrap/js/dropdown.js',
+    //         'source/bootstrap/js/modal.js',
+    //         'source/bootstrap/js/tooltip.js',
+    //         'source/bootstrap/js/popover.js',
+    //         'source/bootstrap/js/scrollspy.js',
+    //         'source/bootstrap/js/tab.js',
+    //         'source/bootstrap/js/affix.js'
+    //       ],
+    //       dest: 'theme/assets/bootstrap.js'
+    //     }
+    // },
   });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Default task(s).
-  grunt.registerTask('default', ['copy','concat']);
+  grunt.registerTask('default', ['copy']);
 };
